fc4221aed39e0341004c27119eac3b723db0e77f
dmnovikov <dj.novikov@gmail.com>
2024-07-04 18:48:56 +0300

    - fix random

---
+++ wemos_mbslave_test/mbregs.h
@@ -0,0 +1,66 @@
+#ifndef __mbregs__
+#define __mbregs__
+
+#include <ModbusRTU.h>
+
+
+#define R_ADDR 0
+#define R_HOUR 1
+#define R_MINS 2
+#define R_SECS 3
+
+
+void fill_hold_regs(ModbusRTU * _mb, int first_reg, int amount){
+  for(int h_reg=first_reg; h_reg< amount; h_reg++){
+      _mb->Hreg(h_reg,random(1,32000));
+      //mb.Hreg(h_reg,ESP8266TrueRandom.random(32000));    //get too long answer, this is bad
+
+      //mb.Hreg(h_reg,5);
+   }
+}
+
+void fill_coil_regs(ModbusRTU * _mb,int first_reg, int amount){
+  for(int h_reg=first_reg; h_reg< amount; h_reg++){
+      _mb->Coil(h_reg,random(0,1));
+      //mb.Hreg(h_reg,ESP8266TrueRandom.random(32000));    //get too long answer, this is bad
+
+      //mb.Hreg(h_reg,5);
+   }
+}
+
+
+void update_regs(ModbusRTU * _mb, int r_amount, int c_amount){
+  uint32_t sec = millis() / 1000ul;      // полное количество секунд со старта платы
+  uint16_t timeHours = (sec / 3600ul);        // часы
+  uint16_t timeMins = (sec % 3600ul) / 60ul;  // минуты
+  uint16_t timeSecs = (sec % 3600ul) % 60ul;  // секунды
+
+  // заполняем реги значениями времени
+  //_mb->Hreg(R_ADDR, modbus_address); 
+  _mb->Hreg(R_HOUR, timeHours);
+  _mb->Hreg(R_MINS, timeMins);
+  _mb->Hreg(R_SECS, timeSecs);
+
+  //next regs are random
+
+  if(r_amount!=0) fill_hold_regs(_mb,R_SECS+1,r_amount-R_SECS);  
+  
+  if(c_amount!=0) fill_coil_regs(_mb, 0,c_amount); 
+
+} 
+
+void init_hold_regs(ModbusRTU * _mb, int first_reg, int amount){
+  for(int h_reg=first_reg; h_reg<amount; h_reg++){
+    _mb->addHreg(h_reg); //add register
+    _mb->Hreg(h_reg,0);  //add 0 to each reg
+  }
+}
+
+void init_coil_regs(ModbusRTU * _mb, int first_reg, int amount){
+  for(int h_reg=first_reg; h_reg<amount; h_reg++){
+    _mb->addCoil(h_reg); //add register
+    _mb->Coil(h_reg,0);  //add 0 to each reg
+  }
+}
+
+#endif
--- wemos_mbslave_test/wemos_mbslave_test.ino
+++ wemos_mbslave_test/wemos_mbslave_test.ino
@@ -1,8 +1,9 @@
 #include <ModbusRTU.h>
 #include <EEPROM.h>
 #include <Ticker.h>
-#include <ESP8266TrueRandom.h>
+// #include <ESP8266TrueRandom.h>
 #include "mbslave.h"
+#include "mbregs.h"
 
 #define DEBUG 1
 
@@ -32,11 +33,6 @@ SerialConfig serial_settings=DEFAULT_MB_FC;
 #define DEFAULT_ADDRESS 126
 #define DEFAULT_INT_REGS 10
 #define DEFAULT_COIL_REGS 10
-
-#define R_ADDR 0
-#define R_HOUR 1
-#define R_MINS 2
-#define R_SECS 3
 
 
 ModbusRTU mb;                 // объект для взаимодействия с либой ModbusRTU
@@ -152,26 +148,18 @@ void setup() {
 
   //Serial.begin(serial_baudrate, (uint8_t) serial_settings_num); // инициализация уарт с настройками для Модбас
   Serial.begin(serial_baudrate, serial_settings); // инициализация уарт с настройками для Модбас
-  pinMode(D1, OUTPUT);
-  digitalWrite(D1, HIGH);
+  pinMode(D5, OUTPUT);
+  digitalWrite(D5, HIGH);
   Serial.swap();
 
   mb.begin(&Serial);  //указание порта для модбас
   mb.slave(modbus_address); // указание адреса устройства в протоколе модбас
 
-  for(int h_reg=0; h_reg<intregs_amount; h_reg++){
-    mb.addHreg(h_reg); //add register
-    mb.Hreg(h_reg,0);  //add 0 to each reg
-  }
-
-   for(int c_reg=0; c_reg<coilregs_amount; c_reg++){
-    mb.addCoil(c_reg); //add register
-    mb.Coil(c_reg,0);  //add 0 to each reg
-  }
+  init_hold_regs(&mb,0,intregs_amount);
+  init_coil_regs(&mb,0,coilregs_amount);
 
 //callback when request comes
   mb.onGetHreg(0,cbReadHreg,intregs_amount);
-
   led_mode_setup=0; //finish setup blinking
 }
 
@@ -179,37 +167,13 @@ void loop() {
 
   mb.task(); // слушаем модбас
   //yield();   // отпускаем для обработки Wi-Fi
-  if(softTimer<(millis())) update_regs(); // обновляем регистры по таймеру
+
+  if(softTimer<(millis())){
+     update_regs(&mb, intregs_amount, coilregs_amount); // обновляем регистры по таймеру
+     softTimer= millis() + 500; // перевзводим на полсекунды
+  }
 }
 
-void update_regs(){
-  uint32_t sec = millis() / 1000ul;      // полное количество секунд со старта платы
-  uint16_t timeHours = (sec / 3600ul);        // часы
-  uint16_t timeMins = (sec % 3600ul) / 60ul;  // минуты
-  uint16_t timeSecs = (sec % 3600ul) % 60ul;  // секунды
-
-  // заполняем реги значениями времени
-  mb.Hreg(R_ADDR, modbus_address); 
-  mb.Hreg(R_HOUR, timeHours);
-  mb.Hreg(R_MINS, timeMins);
-  mb.Hreg(R_SECS, timeSecs);
-
-  //next regs are random
-
-  if(intregs_amount!=0)
-   for(int h_reg=R_SECS+1; h_reg<intregs_amount; h_reg++){
-      //mb.Hreg(h_reg,random(1,32000));
-      mb.Hreg(h_reg,ESP8266TrueRandom.random(32000));
-   }
-  
-  if(coilregs_amount!=0)
-   for(int c_reg=0; c_reg<coilregs_amount; c_reg++){
-     mb.Coil(c_reg,ESP8266TrueRandom.randomBit());
-   }
-
-
-  softTimer= millis() + 500; // перевзводим на полсекунды
-} 
 
 void tickf(){
   if(led_mode_setup) {
