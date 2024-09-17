52c3741e275adbbdb1a6b34ed53983974962f77b
dmnovikov <dj.novikov@gmail.com>
2024-09-17 17:18:22 +0500

    -hregs class

---
+++ wemos_mbslave_test/mbmbregs.h
@@ -0,0 +1,86 @@
+#ifndef __mbmbregs__
+#define __mbmbregs__
+
+#include <ModbusRTU.h>
+
+class MBRegsA: public DBase {
+  private:
+   
+   ModbusRTU * _mb;
+   uint hregs_amount;
+   uint cregs_amount;
+    
+ protected:
+   
+
+  public:
+    MBRegs(WMSettings * __s, ModbusRTU * __mb, uint __hregsa, uint __cregsa): DBase(__s) {
+        _mb=__mb;
+        hregs_amount=__hregsa;
+        cregs_amount=__cregsa;
+    };
+
+    void init(DPublisherMqtt* _pub, Queue<pub_events>* _q) {
+        init_hold_regs(0,hregs_amount);
+        init_coil_regs(0,cregs_amount);
+        init_ok = 1;
+    };
+
+    void update_regs(){
+        uint32_t sec = millis() / 1000ul;      // полное количество секунд со старта платы
+        uint16_t timeHours = (sec / 3600ul);        // часы
+        uint16_t timeMins = (sec % 3600ul) / 60ul;  // минуты
+        uint16_t timeSecs = (sec % 3600ul) % 60ul;  // секунды
+
+        // заполняем реги значениями времени
+        //_mb->Hreg(R_ADDR, modbus_address); 
+        _mb->Hreg(R_HOUR, timeHours);
+        _mb->Hreg(R_MINS, timeMins);
+        _mb->Hreg(R_SECS, timeSecs);
+
+        //next regs are random
+
+        if(hregs_amount!=0) fill_hold_regs(_mb,R_SECS+1,hregs_amount-R_SECS);  
+
+        if(cregs_amount!=0) fill_coil_regs(_mb, 0,cregs_amount); 
+
+    };
+
+    void init_hold_regs(int first_reg, int amount){
+        for(int h_reg=first_reg; h_reg<amount; h_reg++){
+            _mb->addHreg(h_reg); //add register
+            _mb->Hreg(h_reg,0);  //add 0 to each reg
+        }
+    };
+
+
+    void init_coil_regs(int first_reg, int amount){
+        for(int h_reg=first_reg; h_reg<amount; h_reg++){
+            _mb->addCoil(h_reg); //add register
+            _mb->Coil(h_reg,0);  //add 0 to each reg
+        }
+    };
+
+
+    void fill_hold_regs(int first_reg, int amount){
+  
+        for(int h_reg=first_reg; h_reg< amount; h_reg++){
+            _mb->Hreg(h_reg,random(1,32000));
+            //mb.Hreg(h_reg,ESP8266TrueRandom.random(32000));    //get too long answer, this is bad
+
+            //_mb->Hreg(h_reg,5);
+        }
+        
+
+    }; 
+
+
+    void fill_coil_regs(int first_reg, int amount){
+        for(int h_reg=first_reg; h_reg< amount; h_reg++){
+            _mb->Coil(h_reg,random(0,1));
+        }
+
+    };
+
+
+#endif
